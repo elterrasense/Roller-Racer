149bda16d6fc0fd2c7b4e1a6357309540f469ffe
Aitor Pérez Romero <perez.romero.aitor@alumnat.copernic.cat>
2023-05-09 10:28:04 +0200

    IA Enemiga
     -IA básica para un enemigo
     -Ajustada la frecuencia de olas en el plano de agua
     -Actualizada plataforma dónde está la bola enemiga para que no caiga al agua

---
+++ Assets/Scripts/EnemyBallController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBallController : MonoBehaviour
+{
+    public float pushForce = 10f; // Fuerza de empuje aplicada al jugador
+    public float speed = 10f;
+    public float minDistance = 5f;
+
+    private Transform player;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody>();
+        //rb.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        // Calcula la distancia al jugador
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        // Si la distancia es menor que la distancia mínima, persigue al jugador
+        if (distanceToPlayer < minDistance)
+        {
+            // Calcula la dirección hacia el jugador
+            Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
+
+            // Mueve la bola enemiga hacia el jugador
+            rb.AddForce(directionToPlayer * speed);
+        }
+    }
+
+    // Detecta si el jugador entra al radio de la bola
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PushBall(other.gameObject);
+        }
+    }
+
+    // Mueve la bola en direccion del jugador
+    private void PushBall(GameObject ball)
+    {
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+        Vector3 direccion = ball.transform.position - transform.position;
+        direccion.Normalize();
+        rb.AddForce(direccion * pushForce, ForceMode.Impulse);
+    }
+}
