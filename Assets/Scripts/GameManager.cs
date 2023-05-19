8316eb03d3bf7d114a4bf93fd36c524a5d12d059
Aitor Pérez Romero <perez.romero.aitor@alumnat.copernic.cat>
2023-05-19 10:09:13 +0200

    Implementado GameManager
    GameManager y menu de pausa funcionales

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,58 +6,57 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public static GameManager gm;
-    public GameObject player;
-    public enum gameStates { Playing, Victory, GameOver };
-    public gameStates gameState = gameStates.Playing;
-    public GameObject mainCanvas;
-    public GameObject victoryCanvas;
-    public TextMeshProUGUI scoreText;
-
-    private BullInventari bullInventari;
-    private int coins;
-    //private isLive live;
-    public bool buttonPressed = false;
+    public GameObject PauseCanvas;
+    private static bool GameIsPaused = false;
+  
 
     void Start()
     {
-        if (gm == null)
-            gm = gameObject.GetComponent<GameManager>();
-        if (player == null)
-        {
-            player = GameObject.FindWithTag("Player");
-        }
-
-        //live = player.GetComponent<isLive> ();
-            gameState = gameStates.Playing;
-        // Desactivamos el Canvas gameOver, just in case.
-        victoryCanvas.SetActive(false);
-        mainCanvas.SetActive(false);
-
-        bullInventari = GameObject.FindGameObjectWithTag("Player").GetComponent<BullInventari>();
+        PauseCanvas.SetActive(false);
         
     }
 
     void Update()
     {
-        coins = bullInventari.Cantidad;
-        switch (gameState)
-        {
-            case gameStates.Playing:
-                Debug.Log("Gamestate PLAYING");
-                Debug.Log("COINS: " + coins);
-                if (coins >= 5)
-                {
-                    gameState = gameStates.Victory;
-                    scoreText.SetText(coins.ToString());
-                    mainCanvas.SetActive(false);
-                    victoryCanvas.SetActive(true);
-                }
-                break;
-            case gameStates.Victory:
-                Debug.Log("Gamestate VICTORY");
-                // nothing
-                break;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        { 
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                PauseGame();
+            }
         }
+    }
+
+    void PauseGame ()
+    {
+        // Pausa el juego
+        PauseCanvas.SetActive(true);  
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        // Continua el juego
+        Time.timeScale = 1;
+        PauseCanvas.SetActive(false);
+        GameIsPaused = false;
+    }
+
+    public void QuitButton()
+    {
+        // Vuelve al menu principal
+        Time.timeScale = 1;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menus");
+    }
+
+    public void TryAgain()
+    {
+        // Carga de nuevo la escena
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
     }
 
 }
